a1aa19b0f6b730f5c3e7b044c3023323bb04c06d
Kane Zheng <kanez@buildingiq.com>
2020-02-03 10:39:12 +1100

    test

--- src/main/java/com/kanezheng/app/sprintbootquartzjobscrud/job/HelloWorldJob.java
+++ src/main/java/com/kanezheng/app/sprintbootquartzjobscrud/job/HelloWorldJob.java
@@ -1,15 +1,21 @@
 package com.kanezheng.app.sprintbootquartzjobscrud.job;
 
-import org.quartz.Job;
-import org.quartz.JobDataMap;
-import org.quartz.JobExecutionContext;
+import org.quartz.*;
 
 public class HelloWorldJob implements Job {
 
     @Override
     public void execute(JobExecutionContext context) {
+
+        Scheduler scheduler = context.getScheduler();
+
+
         JobDataMap dataMap = context.getJobDetail().getJobDataMap();
         String jobName = dataMap.getString("jobName");
-        System.out.println("Hello: "+ jobName);
+        try {
+            System.out.println("Hello this is : "+ jobName + " running in scheduler: "+ scheduler.getSchedulerName());
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
     }
 }
